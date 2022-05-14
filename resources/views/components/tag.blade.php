9481a2f4a4ded7e1d30b21fed059cdaa3802a51f
Josh Torres <josht@omniadigital.io>
2022-05-14 11:35:43 -0700

    - add more attributes to tag component

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,7 +1,10 @@
 @props([
-    'class'
+    'name' => 'tag',
+    'bgColor' => 'neutral',
+    'textColor' => 'base-text-color',
+    'class' => '',
 ])
 
-<span {{ $attributes->merge(['class' => 'inline-flex items-center px-2 py-1 rounded-lg font-medium ' . $class]) }}>
+<span {{ $attributes->merge(['class' => 'inline-flex items-center px-2 py-1 rounded-lg font-medium ' . 'bg-'.$bgColor .' '. 'text-'.$textColor .' '. $class]) }}>
   {{ $slot }}
 </span>
