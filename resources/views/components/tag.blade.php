999a28b5ca50908f007e52540a671d52433da17b
Josh Torres <josht@omniadigital.io>
2022-05-14 11:40:47 -0700

    - add text size to tag component

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -2,9 +2,10 @@
     'name' => 'tag',
     'bgColor' => 'neutral',
     'textColor' => 'base-text-color',
+    'textSize' => 'base',
     'class' => '',
 ])
 
-<span {{ $attributes->merge(['class' => 'inline-flex items-center px-2 py-1 rounded-lg font-medium ' . 'bg-'.$bgColor .' '. 'text-'.$textColor .' '. $class]) }}>
+<span {{ $attributes->merge(['class' => 'inline-flex items-center px-2 py-1 rounded-lg font-medium uppercase ' . 'bg-'.$bgColor .' '. 'text-'.$textColor .' '. 'text-'.$textSize .' '. $class]) }}>
   {{ $slot }}
 </span>
