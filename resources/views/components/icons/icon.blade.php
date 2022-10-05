7944a8b19d409a712a36447d4871b8665a6f68ac
Josh Torres <josht@omniadigital.io>
2022-10-05 09:12:55 -0700

    - add icon component that will switch off using heroicons or fontawesome based on the name passed in

---
+++ resources/views/components/icons/icon.blade.php
@@ -0,0 +1,15 @@
+@props([
+    'name' => 'name',
+    'size' => 'w-6 h-6',
+    'color' => '',
+])
+
+@if(Str::contains($name,'heroicon'))
+    <x-dynamic-component
+            :component="$name"
+            class="flex-shrink-0 {{ $size }} {{ $color }} {{ $class }} "
+            aria-hidden="true"
+    />
+@else
+    <i class="{{ $name }} flex-shrink-0 {{ $size }} {{ $color }} {{ $class }}"></i>
+@endif
