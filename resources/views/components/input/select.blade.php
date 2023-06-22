cc414eb276bc45e1539eb79488182a1244c75f33
Josh Torres <josht@omniadigital.io>
2023-06-22 13:00:19 -0700

    - add class param to select

--- resources/views/components/input/select.blade.php
+++ resources/views/components/input/select.blade.php
@@ -1,12 +1,14 @@
 @props([
 'options' => [],
-'placeholder' => 'Select an option'
+'placeholder' => 'Select an option',
+'class' => '',
+'optionClass' => ''
 ])
 
-<select {{ $attributes->merge(['class' => 'mt-1 block w-full py-2 px-3 border border-gray-300 bg-neutral rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary sm:text-sm']) }}>
+<select {{ $attributes->merge(['class' => 'mt-1 block w-full py-2 px-3 border border-gray-300 bg-neutral rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary sm:text-sm ' . $class]) }}>
     <option value="" disabled selected>{{ $placeholder }}</option>
 
     @foreach ($options as $key => $value)
-        <option value="{{ $key }}">{{ $value }}</option>
+        <option value="{{ $key }}" class="{{ $optionClass }}">{{ $value }}</option>
     @endforeach
 </select>
